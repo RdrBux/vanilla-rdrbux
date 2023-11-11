45c08dc310b3750b2d1957210dfe3345d28c542b
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2023-11-10 21:29:30 -0300

    Add hero, navbar, work and contact layout. Add colors

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        lightgreen: '#9FE870',
+        darkgreen: '#163300',
+        'content-primary': '#0E0F0C',
+        'content-secondary': '#454745',
+        'content-tertiary': '#6A6C6A',
+        'content-link': '#163300',
+        'interactive-primary': '#163300',
+        'interactive-accent': '#9FE870',
+        'interactive-hover': '#80E142',
+        'interactive-secondary': '#868685',
+        'background-neutral': '#16330014',
+        'border-neutral': '#0E0F0C1F',
+        'base-dark': '#121511',
+      },
+    },
+  },
+  plugins: [],
+};
