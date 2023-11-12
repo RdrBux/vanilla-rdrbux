c955607b20295207f8b3c98bf487e96e82e50721
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2023-11-11 21:47:39 -0300

    Add lenis

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const lenis = new Lenis();
+
+/* lenis.on('scroll', (e) => {
+  console.log(e);
+}); */
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
