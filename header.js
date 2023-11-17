d3510904584461a98bdcf82c86dd359ed1d3c4d3
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2023-11-17 16:31:28 -0300

    Add hero and nav navigation on load

--- header.js
+++ header.js
@@ -4,12 +4,90 @@ const letters = document.querySelectorAll('.letter');
 const smallLetters = document.querySelectorAll('.smletter');
 const header = document.querySelector('header');
 const smallLetterContainer = document.querySelector('.small-container');
+const heroText = document.querySelector('.hero-text');
+const nav = document.querySelector('nav');
+const navItems = document.querySelectorAll('nav li');
 
-let lastScroll = 0;
 const sectionHeight = 60;
+console.log(letters[0]);
+
+window.addEventListener('load', init);
+
+function init() {
+  scroll(0, -1);
+
+  const startOrder = [
+    [letters[2], letters[3]],
+    [letters[1], letters[4]],
+    [letters[0], letters[5]],
+  ];
+
+  const tl = gsap.timeline({
+    defaults: {
+      ease: 'power4.out',
+    },
+  });
+
+  tl.from(header, {
+    autoAlpha: 0,
+  })
+    .from(startOrder[0], {
+      y: '120%',
+      duration: 2,
+    })
+    .from(
+      startOrder[1],
+      {
+        y: '120%',
+        duration: 2,
+      },
+      '<0.2'
+    )
+    .from(
+      startOrder[2],
+      {
+        y: '120%',
+        duration: 2,
+      },
+      '<0.4'
+    )
+    .from(
+      heroText,
+      {
+        autoAlpha: 0,
+        y: '20%',
+        duration: 3,
+      },
+      1
+    )
+    .from(
+      nav,
+      {
+        autoAlpha: 0,
+        scaleX: 0,
+        duration: 0.5,
+      },
+      1.5
+    )
+    .from(
+      navItems,
+      {
+        opacity: 0,
+        duration: 2,
+        stagger: 0.2,
+      },
+      1.7
+    );
+}
 
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
+
+  const orderPairs = [
+    [2, 3],
+    [1, 4],
+    [0, 5],
+  ];
 
   if (scrollY > 350) {
     header.style.position = 'absolute';
@@ -17,11 +95,6 @@ window.addEventListener('scroll', () => {
     header.style.position = 'fixed';
   }
 
-  const orderPairs = [
-    [2, 3],
-    [1, 4],
-    [0, 5],
-  ];
   orderPairs.forEach((pair, orderIndex) => {
     const startScroll = sectionHeight * orderIndex;
 
